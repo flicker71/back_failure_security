ee13cd49fa26548208baaa8160ea746f6d638293
flicker71 <72727849+flicker71@users.noreply.github.com>
2022-06-19 03:53:39 +0200

    script sql + readme

--- README.md
+++ README.md
@@ -20,3 +20,31 @@ j'ai enlevé plusieurs éléments de la bdd donc il permet juste d'accéder à l
 L'identifiant et le password (l'injection) est au dessus de l'input
 
 Pour le CRSF, je suis sur une SPA donc je n'ai pas réussi à le faire.
+
+Résultat de la commande 'npm list'
+├── @prisma/client@3.15.2
+├── bcrypt@5.0.1
+├── body-parser@1.20.0
+├── cors@2.8.5
+├── dotenv@16.0.1
+├── express@4.18.0
+├── http-errors@2.0.0
+├── jsonwebtoken@8.5.1
+├── mysql@2.18.1
+├── nodemon@2.0.16
+├── prisma@3.15.2
+├── ts-node@10.7.0
+└── typescript@4.6.2
+
+résultat de npm list -g
+├── @angular/cli@13.2.4
+├── @nestjs/cli@8.2.5
+├── create-react-app@5.0.0
+├── expo-cli@5.4.3
+├── express-generator@4.16.1
+├── node-gyp@9.0.0
+├── nodemon@2.0.15
+├── sequelize-cli@6.4.1
+├── typeorm@0.2.45
+└── yarn@1.22.17
+
---
+++ security_web.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : dim. 19 juin 2022 à 01:52
+-- Version du serveur : 5.7.36
+-- Version de PHP : 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `security_web`
+--
+CREATE DATABASE IF NOT EXISTS `security_web` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `security_web`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `lessons`
+--
+
+DROP TABLE IF EXISTS `lessons`;
+CREATE TABLE IF NOT EXISTS `lessons` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name_lesson` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `lessons`
+--
+
+INSERT INTO `lessons` (`id`, `name_lesson`, `content`) VALUES
+(1, 'test1', 'Tests numéro 1'),
+(2, 'test2', 'Tests numéro 2'),
+(3, 'test3', 'Test numéro 3');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mail` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `created_at`, `mail`, `password`, `username`) VALUES
+(1, 'Moustique', 'De merde', '19/06/2022', 'çagratte@gmail.com', 'test', 'PiquéLe (cf date de création)'),
+(2, 'Jean', 'aieudeux', '19/06/2022', 'cheh@gmail.com', 'test', 'JeSuiscontent');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
